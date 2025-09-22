5ed7c5ba426fec89cf822f93960f12ebf61298c8
Ryuzo911 <runfromback911@gmail.com>
2025-09-23 01:32:50 +0800

    Add PermissionController

---
+++ app/Http/Controllers/API/PermissionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        return response()->json([
+            'role' => $user->role,
+            'permissions' => [
+                'manage_product' => $user->can('create', Product::class),
+                'manage_category' => $user->can('create', Category::class),
+                'manage_transaction' => $user->can('create', Transaction::class) 
+            ]
+        ]);
+    }
+}
